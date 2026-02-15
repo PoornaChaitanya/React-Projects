219c84898a26ee367929186523a0174e3488096b
PoornaChaitanya <poornachaitanya4259@gmail.com>
2026-02-15 18:42:33 +0530

    Add assets and implement Footer, Navbar, and Home components
    - Added Login page with form handling for sign in and sign up functionality.

--- Netflix Clone/src/App.jsx
+++ Netflix Clone/src/App.jsx
@@ -1,7 +1,15 @@
-import "./App.css";
+import React from "react";
+import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+import { Routes, Route } from "react-router-dom";
 
-function App() {
-  return <></>;
-}
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  );
+};
 
 export default App;
