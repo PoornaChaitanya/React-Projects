012d183015b2405c30fecce4867009d193f9208d
PoornaChaitanya <poornachaitanya4259@gmail.com>
2026-02-15 19:05:46 +0530

    Fetch movie data for TitleCards component and update Home page to include categories

--- Netflix Clone/src/components/TitleCards/TitleCards.jsx
+++ Netflix Clone/src/components/TitleCards/TitleCards.jsx
@@ -1,9 +1,18 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./TitleCards.css";
-import cards_data from "../../assets/cards/Cards_data";
 
 const TitleCards = ({ title, category }) => {
+  const [apiData, setApiData] = useState([]);
   const cardsRef = useRef();
+
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxYjVjMDYxYTk1M2JkNWYwODA3MzMxMDZkODg0MzhiMCIsIm5iZiI6MTc3MTE2MDQ5MS45NjcsInN1YiI6IjY5OTFjM2FiNGMxYTMyNGQ2MjEyNDE2YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.gMRFtR3LSSRuDVUg1vPNkvbBEBL5EJEM2j5Tc0eDG1w",
+    },
+  };
 
   const handleWheel = (event) => {
     event.preventDefault();
@@ -12,17 +21,28 @@ const TitleCards = ({ title, category }) => {
   };
 
   useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/${category ? category : "now_playing"}?language=en-US&page=1`,
+      options,
+    )
+      .then((res) => res.json())
+      .then((res) => setApiData(res.results))
+      .catch((err) => console.error(err));
+
     cardsRef.current.addEventListener("wheel", handleWheel);
   }, []);
   return (
     <div className="title-cards">
       <h2>{title ? title : "Popular on Netflix"}</h2>
       <div className="card-list" ref={cardsRef}>
-        {cards_data.map((card, index) => {
+        {apiData.map((card, index) => {
           return (
             <div className="card" key={index}>
-              <img src={card.image} alt="" />
-              <p>{card.name}</p>
+              <img
+                src={`https://image.tmdb.org/t/p/w500` + card.backdrop_path}
+                alt="movie poster"
+              />
+              <p>{card.original_title}</p>
             </div>
           );
         })}
